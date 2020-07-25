940a94f655013a517da8b5e20a434f37c7574f66
MarkLemashov <48499548+MarkLemashov@users.noreply.github.com>
2020-07-25 21:56:18 +0300

    Updates

---
+++ precache-manifest.3a0e0c55cf20c7df9d4da1bcc622e419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080d3193ce57ede3f55f51a4f694afdb",
+    "url": "/Face-recognition-project/index.html"
+  },
+  {
+    "revision": "d76b32fdc5b1d947f60a",
+    "url": "/Face-recognition-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a8e26c68d4dda12580d",
+    "url": "/Face-recognition-project/static/js/2.2297a8df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-recognition-project/static/js/2.2297a8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76b32fdc5b1d947f60a",
+    "url": "/Face-recognition-project/static/js/main.88a27f44.chunk.js"
+  },
+  {
+    "revision": "a6735c4153b0718931ff",
+    "url": "/Face-recognition-project/static/js/runtime-main.b3fb7d41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Face-recognition-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
