54e980a7e4dc7793e45a4ef7f9d54cd481564c23
MarkLemashov <48499548+MarkLemashov@users.noreply.github.com>
2020-09-07 17:34:30 +0300

    update

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -3,12 +3,12 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return (
-        <div>
+        <div className='taalul'>
             <p className='f3'>
                 {'Creedies first project. OMG COOL!'}
             </p>
-            <div className='center'>
-                <div className='center pa4 br3 shadow-3 form'>
+            <div className='center taalul'>
+                <div className='center pa4 br3 shadow-3 form taalul'>
                     <input className="f4 pa2 w-70 center" type="text" />
                     <button className="w-30 f4 link ph3 pv2 dib grow white bg-light-purple">Detect</button>
                 </div>
