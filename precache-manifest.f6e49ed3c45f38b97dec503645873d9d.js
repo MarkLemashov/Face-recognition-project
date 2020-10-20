1b98ff0845874eb0bdea7a90e599c3139983f3f5
MarkLemashov <48499548+MarkLemashov@users.noreply.github.com>
2020-10-20 22:29:27 +0300

    Updates

---
+++ precache-manifest.f6e49ed3c45f38b97dec503645873d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e06004b531c32d3b3b53fa7d62ddc6f",
+    "url": "/Face-recognition-project/index.html"
+  },
+  {
+    "revision": "4e0ac2def411b98816fb",
+    "url": "/Face-recognition-project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a6d4f4b8b0443cf1f279",
+    "url": "/Face-recognition-project/static/css/main.96492b42.chunk.css"
+  },
+  {
+    "revision": "4e0ac2def411b98816fb",
+    "url": "/Face-recognition-project/static/js/2.a106e63f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-recognition-project/static/js/2.a106e63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d4f4b8b0443cf1f279",
+    "url": "/Face-recognition-project/static/js/main.9a54dc8c.chunk.js"
+  },
+  {
+    "revision": "a6735c4153b0718931ff",
+    "url": "/Face-recognition-project/static/js/runtime-main.b3fb7d41.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Face-recognition-project/precache-manifest.e6e1a55cb44b413460468ce9bfc9cf81.js"
+  "/Face-recognition-project/precache-manifest.f6e49ed3c45f38b97dec503645873d9d.js"
 );
 
 self.addEventListener('message', (event) => {
