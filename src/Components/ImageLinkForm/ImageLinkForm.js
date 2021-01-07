12520151f815380cfc5b18cd1ca7925f5024ca0c
MarkLemashov <marklemashov049@gmail.com>
2021-01-07 12:31:24 +0200

    better flexibility for small screens

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -3,12 +3,12 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = (props) => {
     return (
-        <div>
+        <div className='image_link'>
             <p className='f3'>
                 {props.predictions}
             </p>
             <div className='center'>
-                <div className='center pa4 br3 shadow-3 form'>
+                <div className='center pa4 br3 shadow-3 form shrinks'>
                     <input className="f4 pa2 w-70 center" type="text" onChange={props.onInputChange}/>
                     <button className="w-30 f4 link ph3 pv2 dib grow white bg-light-purple" onClick={props.onDetectPress}>Detect</button>
                 </div>
